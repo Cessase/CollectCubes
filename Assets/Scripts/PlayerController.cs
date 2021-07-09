5d908b7d739e5f7957b13c6a300cbdf12373161a
Yunus Esen <yunusesen@protonmail.com>
2021-07-09 22:54:06 +0300

    initial commit
    Environment and movement scripts added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float rotationSpeed;
+    
+    private Rigidbody player;
+
+    private Quaternion rotationTo;
+    private Vector3 direction;
+    
+    private float horizontalInput;
+    private float verticalInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        if (direction != Vector3.zero)
+        {
+            rotationTo = Quaternion.LookRotation(direction);
+        }
+
+        player.velocity = direction * speed;
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotationTo, Time.deltaTime * rotationSpeed);
+        
+        
+    }
+}
